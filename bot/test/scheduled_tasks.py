a005674ef778a3418d5dc2bdb0bf3044dd1360ff
nadro-j <n4dro@protonmail.com>
2024-09-30 01:45:31 +0000

    scheduled task poc updated to latest

--- bot/test/scheduled_tasks.py
+++ bot/test/scheduled_tasks.py
@@ -1,10 +1,12 @@
 import os
-from datetime import datetime
+from datetime import datetime, timezone
 from dotenv import load_dotenv
 import random
 import asyncio
 import discord
 from discord.ext import commands, tasks
+from discord.ext.tasks import Loop
+from asyncio import coroutine
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -21,114 +23,123 @@ def get_timestamp():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
-def evaluate_task_schedule(task1, task2, minutes=2):
+async def evaluate_task_schedule(self, task: Loop, minutes: int = 2) -> bool:
     """
-    Output the next run times for the given tasks and check if they are within 2 minutes of each other.
+    Evaluates the given task and checks whether the task's next iteration is scheduled
+    within a specific time gap. By default, this function checks if the next iteration of
+    the task is within 2 minutes from the current time.
 
     Args:
-        task1: The first task to check.
-        task2: The second task to check.
+        task (discord.ext.tasks.Loop): The task to evaluate.
+        minutes (int, optional): The time gap in minutes to check. Default is 2 minutes.
+
+    Returns:
+        bool: True if the next iteration of the task is within the given minutes from
+              the current time, else False.
+
+    Note: This function also has a side-effect of stopping the task if the next iteration
+    of the task is within the given minutes from the current time.
     """
-    task1_next_run = task1.next_iteration
-    task2_next_run = task2.next_iteration
+    current_time_utc = datetime.now(timezone.utc)
 
-    if task1_next_run and task2_next_run:
-        task1_str = task1_next_run.strftime('%Y-%m-%d %H:%M:%S')
-        task2_str = task2_next_run.strftime('%Y-%m-%d %H:%M:%S')
-
-        print(f"* {task2.get_task().get_name()} next schedule: {task2_str}")
-        print(f"* {task1.get_task().get_name()} next schedule: {task1_str}")
+    if current_time_utc and task.next_iteration:
 
         # Check if the runs are within 2 minutes (default) of each other
-        time_difference = abs((task1_next_run - task2_next_run).total_seconds())
+        time_difference = abs((current_time_utc - task.next_iteration).total_seconds())
+
         if time_difference <= minutes * 60:
-            print(f"{get_timestamp()} - The tasks are scheduled within 20 minutes of each other. Time difference: {time_difference / 60} minutes.")
+            print(f"The tasks are scheduled within 20 minutes of each other. Time difference: {time_difference / 60:.2f} minutes.")
+            await self.stop_tasks([task])
             return True
 
 
+@staticmethod
 async def stop_tasks(coroutine_task):
     """
     Stops specified asynchronous tasks if they are currently running.
+
+    This function iterates through a list of predefined tasks. For each task, it checks if the task is running and, if so, attempts to stop it.
     """
     for task in coroutine_task:
         try:
             if task.is_running():
                 task = task.get_task()
-                print(f"{get_timestamp()} - Stopping {task.get_name()}, please wait...")
+                print(f"Stopping tasks [{task.get_name()}]")
                 task.cancel()
-                await asyncio.wait([task])
         except Exception as e:
-            task_name = task.get_task().get_name() if task.get_task() else "Unknown"
-            print(f"Error stopping {task_name} task: {e}")
+            print(f"Error stopping {task.get_task().get_name()} task: {e}")
 
 
+@staticmethod
 async def start_tasks(coroutine_task):
     """
     Restarts specified asynchronous tasks if they are not already running.
+
+    This function iterates through a list of predefined tasks. For each task, it checks if the task is not running and, if so, attempts to start it. It logs the start of each task. If an exception occurs while starting a task, it logs the error.
     """
     for task in coroutine_task:
         try:
             if not task.is_running():
-                print(f"{get_timestamp()} - Starting task, please wait...")
                 task.start()
         except Exception as e:
-            task_name = task.get_task().get_name() if task.get_task() else "Unknown"
-            print(f"Error starting {task_name} task: {e}")
+            print(f"Error starting {task.get_task().get_name()} task: {e}")
 
 
-@tasks.loop(hours=3)
+@tasks.loop(minutes=3)
 async def check_governance():
     try:
-        await stop_tasks(coroutine_task=[sync_embeds, recheck_proposals])
+        print("\n")
+        await evaluate_task_schedule(autonomous_voting)
+        await stop_tasks([sync_embeds, recheck_proposals])
         task_name = check_governance.get_task().get_name()
         print(f"{get_timestamp()} - [{task_name}] is running")
-
-        evaluated_time = evaluate_task_schedule(autonomous_voting, check_governance)
-        if evaluated_time:
-            await stop_tasks([autonomous_voting])
-
-        await asyncio.sleep(20)
+        await asyncio.sleep(3)
+        print(f"{get_timestamp()} - 1 new proposal found")
+        await asyncio.sleep(random.uniform(0.1, 25.0))
+        print(f"{get_timestamp()} - Thread created")
+        await asyncio.sleep(random.uniform(0.1, 5.0))
     finally:
-        await start_tasks([autonomous_voting])
+        await start_tasks([autonomous_voting, sync_embeds, recheck_proposals])
+        print("\n")
 
 
-@tasks.loop(hours=6)
+@tasks.loop(minutes=12)
 async def autonomous_voting():
     try:
-        await stop_tasks(coroutine_task=[sync_embeds, recheck_proposals])
+        await stop_tasks([sync_embeds, recheck_proposals])
         task_name = autonomous_voting.get_task().get_name()
         print(f"{get_timestamp()} - [{task_name}] is running")
-        for i in range(0, 60):
-            print(f"Voting on: {i}")
-            await asyncio.sleep(random.uniform(0.1, 2.0))
+        for i in range(0, 5):
+            print(f"{get_timestamp()} - Voting on: {i}")
+            await asyncio.sleep(random.uniform(0.1, 5.0))
     finally:
         await start_tasks([sync_embeds, recheck_proposals])
+        print("\n")
 
 
-@tasks.loop(hours=2)
+@tasks.loop(minutes=1)
 async def sync_embeds():
     try:
-        await stop_tasks(coroutine_task=[recheck_proposals])
+        await stop_tasks([recheck_proposals])
         task_name = sync_embeds.get_task().get_name()
         print(f"{get_timestamp()} - [{task_name}] is running.")
-        for i in range (0, 60):
-            print(f"Synchronizing: {i}")
-            await asyncio.sleep(random.uniform(0.1, 2.0))
+        for i in range (0, 5):
+            print(f"{get_timestamp()} - Synchronizing: {i}")
+            await asyncio.sleep(random.uniform(0.1, 5.0))
     finally:
         await start_tasks([recheck_proposals])
+        print("\n")
 
 
-@tasks.loop(hours=1)
+@tasks.loop(minutes=1)
 async def recheck_proposals():
-    try:
-        task_name = recheck_proposals.get_task().get_name()
-        print(f"{get_timestamp()} - [{task_name}] is running")
-        for i in range (0, 60):
-            print(f"Rechecking proposal: {i}")
-            await asyncio.sleep(random.uniform(0.1, 1))
-    finally:
-        pass
+    task_name = recheck_proposals.get_task().get_name()
+    print(f"{get_timestamp()} - [{task_name}] is running")
 
+    for i in range (0, 5):
+        print(f"{get_timestamp()} - Rechecking proposal: {i}")
+        await asyncio.sleep(random.uniform(0.1, 5.0))
+    print("\n")
 
 @autonomous_voting.before_loop
 async def before_voting():
